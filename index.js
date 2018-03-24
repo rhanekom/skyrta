aeca682e2e41325e6c40d6d78ee010901a5d39b2
Riaan Hanekom <rhanekom@gmail.com>
2018-03-24 14:14:17 +0200

    Minor refactoring of index.js to prepare for seperation of a runner

--- index.js
+++ index.js
@@ -9,6 +9,15 @@ module.exports = (function() {
     let langs = new SupportedLanguages();
 
     function generate(language, source)  {
+        let executable = validate(language, source); 
+        return execute(executable, source);        
+    }
+
+    function supportedLanguages() {
+        return langs;
+    }
+
+    function validate(language, source) {
         let executable = langs.getCommand(language);
 
         if (!executable) {
@@ -17,25 +26,24 @@ module.exports = (function() {
         
         if (!source) {
             throw new Error('No source provided for input');
-        } 
+        }
 
-        const pipedprocess = new PipedProcess();
-        
-        try {  
+        return executable;
+    }
+    
+    function execute(executable, source) {
+        try {
+            const pipedprocess = new PipedProcess();  
             let output = pipedprocess.run(executable.exec, executable.args, source);
             
             if (output) {
                 return new Svg(output);
             }
-
+    
             return null;
         } catch (e) {                        
-            throw new Error(`Unable to render ${language} graph: ${e}`);
-        }        
-    }
-
-    function supportedLanguages() {
-        return langs;
+            throw new Error(`Unable to render graph with ${executable.exec}: ${e}`);
+        }
     }
 
     return {
