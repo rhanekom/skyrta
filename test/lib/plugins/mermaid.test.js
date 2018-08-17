e6dc06f094a7a0539bdd6e5bc9ff4a9b303976b4
Riaan Hanekom <rhanekom@gmail.com>
2018-08-17 17:49:08 +0200

    Puppeteer config file to remove sandboxing for the purposes of tests

--- test/lib/plugins/mermaid.test.js
+++ test/lib/plugins/mermaid.test.js
@@ -4,19 +4,19 @@ var mermaid = require('../../../lib/plugins/mermaid');
 const tinyGraph = 'classDiagram\r\nClass01 <|-- AveryLongClass : Cool';
 
 test('generates valid svg graph', () => {
-    let svg = skyrta.generate('mermaid', tinyGraph);
+  let svg = skyrta.generate('mermaid', tinyGraph, { 'puppeteerConfigFile': 'test/puppeteer-config.json' });
     expect(svg.value).toMatch(/<svg[\s\S]*>[\s\S]*<\/svg>/m);
 });
 
 test('generates valid svg graph with options', () => {
-    let svg = skyrta.generate('mermaid', tinyGraph, { theme: 'forest', backgroundColor: 'white'});
+  let svg = skyrta.generate('mermaid', tinyGraph, { theme: 'forest', backgroundColor: 'white', 'puppeteerConfigFile': 'test/puppeteer-config.json' });
     expect(svg.value).toMatch(/<svg[\s\S]*>[\s\S]*<\/svg>/m);
 });
 
 
 test('getCommand has correct executable for mermaid', () => {
     const cmd = mermaid.getCommand(tinyGraph);
-    expect(cmd.exec).toContain('mmdc');    
+    expect(cmd.exec).toContain('mmdc');
 });
 
 test('getCommand has input and output parameters for generation', () => {
@@ -30,42 +30,42 @@ test('getCommand has input and output parameters for generation', () => {
 test('options theme is translated to --theme', () => {
     const cmd = mermaid.getCommand(tinyGraph, { theme: 'default'});
     expect(cmd.args).toContain('--theme');
-    expect(cmd.args).toContain('default');    
+    expect(cmd.args).toContain('default');
 });
 
 
 test('options width is translated to --width', () => {
     const cmd = mermaid.getCommand(tinyGraph, { width: 800});
     expect(cmd.args).toContain('--width');
-    expect(cmd.args).toContain('800');    
+    expect(cmd.args).toContain('800');
 });
 
 test('options height is translated to --height', () => {
     const cmd = mermaid.getCommand(tinyGraph, { height: 600});
     expect(cmd.args).toContain('--height');
-    expect(cmd.args).toContain('600');    
+    expect(cmd.args).toContain('600');
 });
 
 test('options backgroundColor is translated to --backgroundColor', () => {
     const cmd = mermaid.getCommand(tinyGraph, { backgroundColor: 'white'});
     expect(cmd.args).toContain('--backgroundColor');
-    expect(cmd.args).toContain('white');    
+    expect(cmd.args).toContain('white');
 });
 
 test('options configFile is translated to --configFile', () => {
     const cmd = mermaid.getCommand(tinyGraph, { configFile: 'config.js'});
     expect(cmd.args).toContain('--configFile');
-    expect(cmd.args).toContain('config.js');    
+    expect(cmd.args).toContain('config.js');
 });
 
 test('options cssFile is translated to --cssFile', () => {
     const cmd = mermaid.getCommand(tinyGraph, { cssFile: 'styles.css'});
     expect(cmd.args).toContain('--cssFile');
-    expect(cmd.args).toContain('styles.css');    
+    expect(cmd.args).toContain('styles.css');
 });
 
 test('options puppeteerConfigFile is translated to --puppeteerConfigFile', () => {
     const cmd = mermaid.getCommand(tinyGraph, { puppeteerConfigFile: 'puppeteer.js'});
     expect(cmd.args).toContain('--puppeteerConfigFile');
-    expect(cmd.args).toContain('puppeteer.js');    
+    expect(cmd.args).toContain('puppeteer.js');
 });
